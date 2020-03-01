e13eb59f716962e1a67423ba8088065b7849d296
tuanminh160302 <canhtuan09@gmail.com>
2020-03-01 17:13:43 +0700

    #1 commit 3.1.20

--- js/scrollMagic.js
+++ js/scrollMagic.js
@@ -1,33 +1,51 @@
-var tl = new TimelineMax({onUpdate:updatePercentage});
-var tl2 = new TimelineMax();
-const controller = new ScrollMagic.Controller();
+$(document).ready(function() {
 
-tl.from('blockquote', .5, {x:200, opacity: 0});
-tl.from('span', 1, { width: 0}, "=-.5");
-tl.from('#office', 1, {x:-200, opacity: 0,ease: Power4.easeInOut}, "=-1");
-tl.from('#building', 1, {x:200, opacity: 0, ease: Power4.easeInOut}, "=-.7");
+  var curPage = 1;
+  var numOfPages = $(".skw-page").length;
+  var animTime = 1000;
+  var scrolling = false;
+  var pgPrefix = ".skw-page-";
 
-tl2.from("#box", 1, {opacity: 0, scale: 0});
-tl2.to("#box", .5, {left: "20%", scale: 1.3, borderColor: 'white', borderWidth: 12, boxShadow: '1px 1px 0px 0px rgba(0,0,0,0.09)'})
+  function pagination() {
+    scrolling = true;
 
-const scene = new ScrollMagic.Scene({
-  triggerElement: ".sticky",
-            triggerHook: "onLeave",
-            duration: "100%"
-})
-  .setPin(".sticky")
-  .setTween(tl)
-		.addTo(controller);
+    $(pgPrefix + curPage).removeClass("inactive").addClass("active");
+    $(pgPrefix + (curPage - 1)).addClass("inactive");
+    $(pgPrefix + (curPage + 1)).removeClass("active");
 
-const scene2 = new ScrollMagic.Scene({
-  triggerElement: "blockquote"
-})
-  .setTween(tl2)
-		.addTo(controller);
+    setTimeout(function() {
+      scrolling = false;
+    }, animTime);
+  };
 
+  function navigateUp() {
+    if (curPage === 1) return;
+    curPage--;
+    pagination();
+  };
 
-function updatePercentage() {
-  percent.innerHTML = (tl.progress() *100 ).toFixed();
-  tl.progress();
-  console.log(tl.progress());
-}
+  function navigateDown() {
+    if (curPage === numOfPages) return;
+    curPage++;
+    pagination();
+  };
+
+  $(document).on("mousewheel DOMMouseScroll", function(e) {
+    if (scrolling) return;
+    if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+      navigateUp();
+    } else { 
+      navigateDown();
+    }
+  });
+
+  $(document).on("keydown", function(e) {
+    if (scrolling) return;
+    if (e.which === 38) {
+      navigateUp();
+    } else if (e.which === 40) {
+      navigateDown();
+    }
+  });
+
+});
