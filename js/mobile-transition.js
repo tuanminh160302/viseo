d414df147c911f79c97698efa1aa44a14c2ea5c0
tuanminh160302 <canhtuan09@gmail.com>
2020-05-01 18:19:09 +0700

    10

--- js/mobile-transition.js
+++ js/mobile-transition.js
@@ -5,7 +5,6 @@ document.getElementById('trigger-menu').addEventListener('click', function() {
     TweenMax.to('.mmenu-close', .3, { delay: .3, opacity: 1, zIndex: '2000'});
     TweenMax.to('.mtabs', .3, { delay: 1, opacity: 1});
     TweenMax.to('.vertical-mmenu-expand', 1, { scaleY: 1, ease: Power4.easeOut});
-    TweenMax.to('.vertical-mmenu-expand', 0, { delay: 1.5, WebkitTransformOrigin: 'bottom'});
 })
 
 document.getElementById('trigger-close-menu').addEventListener('click', function() {
@@ -16,5 +15,4 @@ document.getElementById('trigger-close-menu').addEventListener('click', function
     TweenMax.to('.mmenu-close', .3, { opacity: 0, zIndex: '1999'});
     TweenMax.to('.mtabs', .3, { opacity: 0});
     TweenMax.to('.vertical-mmenu-expand', 1, { delay: 1, scaleY: 0, ease: Power4.easeOut});
-    TweenMax.to('.vertical-mmenu-expand', 0, { delay: 2, WebkitTransformOrigin: 'top'});
 })
